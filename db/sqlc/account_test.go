d25cfdcc30afc84655bad42b9a3ff49052fa7127
a7medalyapany <ahmedalyapany1@gmail.com>
2026-01-14 04:15:24 +0200

    feat: Add tests for account retrieval, update, and deletion functionalities

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -3,6 +3,7 @@ package db
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/a7medalyapany/GoBank.git/util"
 	"github.com/stretchr/testify/require"
@@ -32,4 +33,48 @@ func createRandomAccount(t *testing.T) Account {
 
 func TestCreateAccount(t *testing.T) {
 	createRandomAccount(t)
+}
+
+func TestGetAccount(t *testing.T) {
+	account := createRandomAccount(t)
+
+	retrievedAccount, err := testQueries.GetAccount(context.Background(), account.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, retrievedAccount)
+
+	require.Equal(t, account.ID, retrievedAccount.ID)
+	require.Equal(t, account.Owner, retrievedAccount.Owner)
+	require.Equal(t, account.Balance, retrievedAccount.Balance)
+	require.Equal(t, account.Currency, retrievedAccount.Currency)
+	require.WithinDuration(t, account.CreatedAt.Time, retrievedAccount.CreatedAt.Time, time.Second)
+}
+
+func TestUpdateAccount(t *testing.T) {
+	account := createRandomAccount(t)
+
+	arg := UpdateAccountParams{
+		ID:      account.ID,
+		Balance: util.RandomMoney(),
+	}
+
+	updatedAccount, err := testQueries.UpdateAccount(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, updatedAccount)
+
+	require.Equal(t, account.ID, updatedAccount.ID)
+	require.Equal(t, arg.Balance, updatedAccount.Balance)
+	require.Equal(t, account.Owner, updatedAccount.Owner)
+	require.Equal(t, account.Currency, updatedAccount.Currency)
+	require.WithinDuration(t, account.CreatedAt.Time, updatedAccount.CreatedAt.Time, time.Second)
+}
+
+func TestDeleteAccount(t *testing.T) {
+	account := createRandomAccount(t)
+
+	err := testQueries.DeleteAccount(context.Background(), account.ID)
+	require.NoError(t, err)
+
+	retrievedAccount, err := testQueries.GetAccount(context.Background(), account.ID)
+	require.Error(t, err)
+	require.Empty(t, retrievedAccount)
 }
