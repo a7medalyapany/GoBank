616e648d3a878c8e371555cfc0a855bdc5ddd538
a7medalyapany <ahmedalyapany1@gmail.com>
2026-01-19 05:29:49 +0200

    feat: Add FormatMoney function for converting pgtype.Numeric to string representation

--- db/sqlc/numeric.go
+++ db/sqlc/numeric.go
@@ -3,6 +3,7 @@ package db
 import (
 	"fmt"
 	"math/big"
+	"strings"
 
 	"github.com/jackc/pgx/v5/pgtype"
 )
@@ -23,4 +24,33 @@ func NegateNumeric(n pgtype.Numeric) (pgtype.Numeric, error) {
 		NaN:   n.NaN,
 		Valid: n.Valid,
 	}, nil
+}
+
+
+// FormatMoney converts a pgtype.Numeric to a string representation
+// with decimal point based on its exponent.
+func FormatMoney(n pgtype.Numeric) string {
+	if !n.Valid || n.Int == nil {
+		return "invalid"
+	}
+
+	// copy to avoid mutating original
+	i := new(big.Int).Set(n.Int)
+
+	if n.Exp >= 0 {
+		i.Mul(i, big.NewInt(0).Exp(big.NewInt(10), big.NewInt(int64(n.Exp)), nil))
+		return i.String()
+	}
+
+	exp := int(-n.Exp)
+	s := i.String()
+
+	if len(s) <= exp {
+		s = "0." + strings.Repeat("0", exp-len(s)) + s
+	} else {
+		pos := len(s) - exp
+		s = s[:pos] + "." + s[pos:]
+	}
+
+	return s
 }
