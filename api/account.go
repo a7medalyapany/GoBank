c5984d9038bec82c663f1a2cb412614e823dd861
a7medalyapany <ahmedalyapany1@gmail.com>
2026-02-12 11:38:51 +0200

    feat: implement deleteAccount function and add route for deleting an account

--- api/account.go
+++ api/account.go
@@ -133,3 +133,32 @@ func (server *Server) updateAccount(ctx *gin.Context) {
 
 	ctx.JSON(http.StatusOK, account)
 }
+
+
+type deleteAccountRequest struct {
+	ID int64 `uri:"id" binding:"required,min=1"`
+}
+func (server *Server) deleteAccount(ctx *gin.Context) {
+	var req deleteAccountRequest
+
+	err := ctx.ShouldBindUri(&req)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+
+	err = server.store.DeleteAccount(ctx, req.ID)
+	if err != nil {
+
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"status": "deleted"})
+}
