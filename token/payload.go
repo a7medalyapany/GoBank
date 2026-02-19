89e72f16e610fc9be15a54ced31b684cd01ab261
a7medalyapany <ahmedalyapany1@gmail.com>
2026-02-19 16:56:42 +0200

    feat: add Payload struct and functions for token management

---
+++ token/payload.go
@@ -0,0 +1,42 @@
+package token
+
+import (
+	"errors"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+var (
+	ErrExpiredToken = errors.New("token has expired")
+	ErrInvalidToken = errors.New("token is invalid")
+)
+
+type Payload struct {
+	ID        uuid.UUID `json:"id"`
+	Username  string    `json:"username"`
+	IssuedAt  time.Time `json:"issued_at"`
+	ExpiredAt time.Time `json:"expired_at"`
+}
+
+func NewPayload(username string, duration time.Duration) (*Payload, error) {
+	tokenID, err := uuid.NewRandom()
+	if err != nil {
+		return nil, err
+	}
+
+	now := time.Now()
+	return &Payload{
+		ID:        tokenID,
+		Username:  username,
+		IssuedAt:  now,
+		ExpiredAt: now.Add(duration),
+	}, nil
+}
+
+func (p *Payload) Valid() error {
+	if time.Now().After(p.ExpiredAt) {
+		return ErrExpiredToken
+	}
+	return nil
+}
