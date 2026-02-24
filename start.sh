2980c67c487f139bd9c3510fac4bd50b67608f10
a7medalyapany <ahmedalyapany1@gmail.com>
2026-02-25 01:23:59 +0200

    feat: add environment variable loading to start script

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-
 set -e
+
+echo "Loading environment variables..."
+export $(cat app.env | xargs)
 
 echo "Running database migrations..."
 /go-bank/migrate -path /go-bank/migration -database "$DB_URL" -verbose up
