278da575168f7655b4b8c4741b09ee35ff5eb33a
a7medalyapany <ahmedalyapany1@gmail.com>
2026-03-10 11:51:57 +0200

    feat: enhance login user validation and error handling

--- gapi/rpc_login_user.go
+++ gapi/rpc_login_user.go
@@ -7,69 +7,72 @@ import (
 	db "github.com/a7medalyapany/GoBank.git/db/sqlc"
 	"github.com/a7medalyapany/GoBank.git/pb"
 	"github.com/a7medalyapany/GoBank.git/util"
+	"github.com/a7medalyapany/GoBank.git/val"
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgtype"
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
-
 func (server *Server) LoginUser(ctx context.Context, req *pb.LoginUserRequest) (*pb.LoginUserResponse, error) {
+	if violations := validateLoginUserRequest(req); violations != nil {
+		return nil, invalidArgumentError(violations)
+	}
 
 	user, err := server.store.GetUser(ctx, req.GetUsername())
 	if err != nil {
 		if errors.Is(err, pgx.ErrNoRows) {
-		return nil, status.Errorf(codes.NotFound, "User not found: %v", err)
+			return nil, status.Errorf(codes.NotFound, "user not found")
 		}
-		return nil, status.Errorf(codes.Internal, "Failed to get user: %v", err)
+		return nil, status.Errorf(codes.Internal, "failed to get user: %v", err)
 	}
 
-	err = util.CheckPassword(req.Password, user.HashedPassword)
-	if err != nil {
-		return nil, status.Errorf(codes.NotFound, "Invalid password: %v", err)
+	if err = util.CheckPassword(req.GetPassword(), user.HashedPassword); err != nil {
+		return nil, status.Errorf(codes.Unauthenticated, "invalid password")
 	}
 
-	accessToken, accessPayload, err := server.tokenMaker.CreateToken(
-		user.Username,
-		server.config.ACCESS_TOKEN_DURATION,
-	)
+	accessToken, accessPayload, err := server.tokenMaker.CreateToken(user.Username, server.config.ACCESS_TOKEN_DURATION)
 	if err != nil {
-		return nil, status.Errorf(codes.Internal, "Failed to create access token: %v", err)
+		return nil, status.Errorf(codes.Internal, "failed to create access token: %v", err)
 	}
 
-	refreshToken, refreshPayload, err := server.tokenMaker.CreateToken(
-		user.Username,
-		server.config.REFRESH_TOKEN_DURATION,
-	)
+	refreshToken, refreshPayload, err := server.tokenMaker.CreateToken(user.Username, server.config.REFRESH_TOKEN_DURATION)
 	if err != nil {
-		return nil, status.Errorf(codes.Internal, "Failed to create refresh token: %v", err)
+		return nil, status.Errorf(codes.Internal, "failed to create refresh token: %v", err)
 	}
 
 	mt := server.extractMetadata(ctx)
-
 	session, err := server.store.CreateSession(ctx, db.CreateSessionParams{
-		ID: pgtype.UUID{Bytes: refreshPayload.ID, Valid: true},
-		Username: user.Username,
+		ID:           pgtype.UUID{Bytes: refreshPayload.ID, Valid: true},
+		Username:     user.Username,
 		RefreshToken: refreshToken,
-		UserAgent: mt.UserAgent,
-		ClientIp: mt.ClientIp,
-		IsBlocked: false,
-		ExpiresAt: pgtype.Timestamptz{Time: refreshPayload.ExpiredAt, Valid: true},
+		UserAgent:    mt.UserAgent,
+		ClientIp:     mt.ClientIp,
+		IsBlocked:    false,
+		ExpiresAt:    pgtype.Timestamptz{Time: refreshPayload.ExpiredAt, Valid: true},
 	})
 	if err != nil {
-		return nil, status.Errorf(codes.Internal, "Failed to create session: %v", err)
+		return nil, status.Errorf(codes.Internal, "failed to create session: %v", err)
 	}
 
-
-	rsp := &pb.LoginUserResponse{
-		SessionId: session.ID.String(),
-		AccessToken: accessToken,
-		RefreshToken: refreshToken,
-		AccessTokenExpiresAt: timestamppb.New(accessPayload.ExpiredAt),
+	return &pb.LoginUserResponse{
+		SessionId:             session.ID.String(),
+		AccessToken:           accessToken,
+		RefreshToken:          refreshToken,
+		AccessTokenExpiresAt:  timestamppb.New(accessPayload.ExpiredAt),
 		RefreshTokenExpiresAt: timestamppb.New(refreshPayload.ExpiredAt),
+		User:                  convertUser(user),
+	}, nil
+}
 
-		User: convertUser(user),
+func validateLoginUserRequest(req *pb.LoginUserRequest) (violations []*errdetails.BadRequest_FieldViolation) {
+	if err := val.ValidateUsername(req.GetUsername()); err != nil {
+		violations = append(violations, fieldViolation("username", err))
 	}
-	return rsp, nil
+	if err := val.ValidatePassword(req.GetPassword()); err != nil {
+		violations = append(violations, fieldViolation("password", err))
+	}
+	return
 }
