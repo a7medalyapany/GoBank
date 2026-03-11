0322057d4f9dea8f72d5b0435c65f0f0cccb5a2c
a7medalyapany <ahmedalyapany1@gmail.com>
2026-03-11 06:38:58 +0200

    fix: separate protoc command and swagger JSON copy in Makefile for clarity

--- db/sqlc/user_test.go
+++ db/sqlc/user_test.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/a7medalyapany/GoBank.git/util"
+	"github.com/jackc/pgx/v5/pgtype"
 	"github.com/stretchr/testify/require"
 )
 
@@ -52,4 +53,108 @@ func TestGetUser(t *testing.T) {
 	require.Equal(t, user.Email, retrievedUser.Email)
 	require.WithinDuration(t, user.PasswordChangedAt.Time, retrievedUser.PasswordChangedAt.Time, time.Second)
 	require.WithinDuration(t, user.CreatedAt.Time, retrievedUser.CreatedAt.Time, time.Second)
+}
+
+func TestUpdateUserFullName(t *testing.T) {
+	oldUser := createRandomUser(t)
+
+	newFullName := util.RandomOwner()
+	updatedUser, err := testQueries.UpdateUser(context.Background(), UpdateUserParams{
+		Username: oldUser.Username,
+		FullName: pgtype.Text{String: newFullName, Valid: true},
+	})
+
+	require.NoError(t, err)
+	require.NotEmpty(t, updatedUser)
+	require.NotEqual(t, oldUser.FullName, updatedUser.FullName)
+	require.Equal(t, newFullName, updatedUser.FullName)
+
+	require.Equal(t, oldUser.Email, updatedUser.Email)
+	require.Equal(t, oldUser.HashedPassword, updatedUser.HashedPassword)
+}
+
+func TestUpdateUserEmail(t *testing.T) {
+	oldUser := createRandomUser(t)
+
+	newEmail := util.RandomEmail()
+	updatedUser, err := testQueries.UpdateUser(context.Background(), UpdateUserParams{
+		Username: oldUser.Username,
+		Email:    pgtype.Text{String: newEmail, Valid: true},
+	})
+
+	require.NoError(t, err)
+	require.NotEmpty(t, updatedUser)
+	require.NotEqual(t, oldUser.Email, updatedUser.Email)
+	require.Equal(t, newEmail, updatedUser.Email)
+
+	require.Equal(t, oldUser.FullName, updatedUser.FullName)
+	require.Equal(t, oldUser.HashedPassword, updatedUser.HashedPassword)
+}
+
+func TestUpdateUserPassword(t *testing.T) {
+	oldUser := createRandomUser(t)
+
+	newHashedPassword, err := util.HashPassword(util.RandomOwner())
+	require.NoError(t, err)
+	updatedUser, err := testQueries.UpdateUser(context.Background(), UpdateUserParams{
+		Username:       oldUser.Username,
+		HashedPassword: pgtype.Text{String: newHashedPassword, Valid: true},
+	})
+
+	require.NoError(t, err)
+	require.NotEmpty(t, updatedUser)
+	require.NotEqual(t, oldUser.HashedPassword, updatedUser.HashedPassword)
+	require.Equal(t, newHashedPassword, updatedUser.HashedPassword)
+
+	require.Equal(t, oldUser.FullName, updatedUser.FullName)
+	require.Equal(t, oldUser.Email, updatedUser.Email)
+}
+
+
+func TestUpdateUserAllFields(t *testing.T) {
+	oldUser := createRandomUser(t)
+
+	newFullName := util.RandomOwner()
+	newEmail := util.RandomEmail()
+	newHashedPassword, err := util.HashPassword(util.RandomOwner())
+	require.NoError(t, err)
+
+	updatedUser, err := testQueries.UpdateUser(context.Background(), UpdateUserParams{
+		Username:       oldUser.Username,
+		FullName:       pgtype.Text{String: newFullName, Valid: true},
+		Email:          pgtype.Text{String: newEmail, Valid: true},
+		HashedPassword: pgtype.Text{String: newHashedPassword, Valid: true},
+	})
+
+	require.NoError(t, err)
+	require.NotEmpty(t, updatedUser)
+	require.NotEqual(t, oldUser.FullName, updatedUser.FullName)
+	require.Equal(t, newFullName, updatedUser.FullName)
+	require.NotEqual(t, oldUser.Email, updatedUser.Email)
+	require.Equal(t, newEmail, updatedUser.Email)
+	require.NotEqual(t, oldUser.HashedPassword, updatedUser.HashedPassword)
+	require.Equal(t, newHashedPassword, updatedUser.HashedPassword)
+}
+
+func TestUpdateUserPasswordChangedAt(t *testing.T) {
+    oldUser := createRandomUser(t)
+
+    require.True(t, oldUser.PasswordChangedAt.Time.IsZero())
+
+    newHashedPassword, err := util.HashPassword(util.RandomOwner())
+    require.NoError(t, err)
+
+    updatedUser, err := testQueries.UpdateUser(context.Background(), UpdateUserParams{
+        Username:           oldUser.Username,
+        HashedPassword:     pgtype.Text{String: newHashedPassword, Valid: true},
+        PasswordChangedAt:  pgtype.Timestamptz{Time: time.Now(), Valid: true},
+    })
+
+    require.NoError(t, err)
+    require.False(t, updatedUser.PasswordChangedAt.Time.IsZero())
+    require.NotEqual(t, oldUser.HashedPassword, updatedUser.HashedPassword)
+    
+
+    require.Equal(t, oldUser.FullName, updatedUser.FullName)
+    require.Equal(t, oldUser.Email, updatedUser.Email)
 }
