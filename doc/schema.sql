54c6d58feac48127e117677a468f09cabcb7c4ca
a7medalyapany <ahmedalyapany1@gmail.com>
2026-03-16 01:19:32 +0200

    feat: add email verification tables and fields to database schema

--- doc/schema.sql
+++ doc/schema.sql
@@ -1,14 +1,25 @@
 -- SQL dump generated using DBML (dbml.dbdiagram.io)
 -- Database: PostgreSQL
--- Generated at: 2026-03-06T04:10:06.339Z
+-- Generated at: 2026-03-15T21:14:01.790Z
 
 CREATE TABLE "users" (
   "username" varchar PRIMARY KEY,
   "hashed_password" varchar NOT NULL,
   "full_name" varchar NOT NULL,
   "email" varchar UNIQUE NOT NULL,
+  "is_email_verified" boolean NOT NULL DEFAULT false,
   "password_changed_at" timestamptz NOT NULL DEFAULT (0001-01-01 00:00:00Z),
   "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "verify_emails" (
+  "id" bigserial PRIMARY KEY,
+  "username" varchar NOT NULL,
+  "email" varchar NOT NULL,
+  "secret_code" varchar NOT NULL,
+  "is_used" boolean NOT NULL DEFAULT false,
+  "created_at" timestamptz NOT NULL DEFAULT (now()),
+  "expires_at" timestamptz NOT NULL DEFAULT (now() + interval '15 minutes')
 );
 
 CREATE TABLE "accounts" (
@@ -62,6 +73,8 @@ CREATE INDEX ON "sessions" ("id");
 COMMENT ON COLUMN "entries"."amount" IS 'can be +ve, or -ve';
 
 COMMENT ON COLUMN "transfers"."amount" IS 'Must be +ve';
+
+ALTER TABLE "verify_emails" ADD FOREIGN KEY ("username") REFERENCES "users" ("username") DEFERRABLE INITIALLY IMMEDIATE;
 
 ALTER TABLE "accounts" ADD FOREIGN KEY ("owner") REFERENCES "users" ("username") DEFERRABLE INITIALLY IMMEDIATE;
 
