30b588454328479523a99f0f8557ed74dd081a39
a7medalyapany <ahmedalyapany1@gmail.com>
2026-04-16 18:33:32 +0200

    feat: add SQL query to retrieve the latest active verification email

--- db/query/verify_email.sql
+++ db/query/verify_email.sql
@@ -3,6 +3,16 @@ INSERT INTO verify_emails (username, email, secret_code)
 VALUES ($1, $2, $3)
 RETURNING *;
 
+-- name: GetLatestActiveVerifyEmail :one
+SELECT *
+FROM verify_emails
+WHERE username = $1
+  AND email = $2
+  AND is_used = false
+  AND expires_at > now()
+ORDER BY created_at DESC
+LIMIT 1;
+
 -- name: UpdateVerifyEmail :one
 UPDATE verify_emails SET is_used = true
 WHERE id = $1
